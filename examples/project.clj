b7f384ddbf6323295007eaf7e9212cd3a3811328
Dave Della Costa <ddellacosta@gmail.com>
2013-05-22 17:24:46 +0900

    bump versions, include friend-oauth2 to 0.0.4

--- examples/project.clj
+++ examples/project.clj
@@ -1,16 +1,23 @@
-(defproject friend-oauth2-examples "0.0.2"
+(defproject friend-oauth2-examples "0.0.3"
   :description "Friend Oauth2 Workflow examples, includes App.net, Facebook and Github handlers."
+
   :url "https://github.com/ddellacosta/friend-oauth2-examples"
+
   :license {:name "MIT License"
             :url "http://dd.mit-license.org"}
+
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.5"]
-                 [com.cemerick/friend "0.1.4"]
-                 [friend-oauth2 "0.0.3"]
-                 [cheshire "5.0.2"]]
-  :plugins [[lein-ring "0.8.3"]]
+                 [compojure "1.1.5" :exclusions [ring/ring-core org.clojure/core.incubator]]
+                 [com.cemerick/friend "0.1.5" :exclusions [ring/ring-core]]
+                 [friend-oauth2 "0.0.4" :exclusions [org.apache.httpcomponents/httpcore]]
+                 [cheshire "5.0.2"]
+                 [ring-server "0.2.8" :exclusions [ring]]]
+
+  :plugins [[lein-ring "0.8.5"]]
+
   :ring {:handler friend-oauth2-examples.appdotnet-handler/app}
 ;;  :ring {:handler friend-oauth2-examples.facebook-handler/app}
 ;;  :ring {:handler friend-oauth2-examples.github-handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+
+  :profiles {:dev
+             {:dependencies [[ring-mock "0.1.3"]]}})
