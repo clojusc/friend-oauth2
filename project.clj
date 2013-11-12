d80c8e29a3a5e4e972dd16d8e76c7c8f3a18802e
Dave Della Costa <ddellacosta@gmail.com>
2013-11-12 23:11:52 +0900

    0.1.0 -> 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject friend-oauth2 "0.1.0"
+(defproject friend-oauth2 "0.1.1"
   :description "OAuth2 workflow for Friend (https://github.com/cemerick/friend). (Bug reports/contributions welcome!)"
   :url "https://github.com/ddellacosta/friend-oauth2"
   :license {:name "MIT License"
