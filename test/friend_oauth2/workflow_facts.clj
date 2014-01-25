5f5515276c426a754caa7223bfe4a83aac395fa0
tutysara <tutysara@gmail.com>
2014-01-25 14:54:29 +0530

    Fix for handling error response from authentication provider

--- test/friend_oauth2/workflow_facts.clj
+++ test/friend_oauth2/workflow_facts.clj
@@ -52,6 +52,29 @@
    (:client_id redirect-query)                           => "my-client-id"
    (nil? (:state redirect-query))                        => false))
 
+(fact
+ "On error response from authentication provider execute error function"
+ 
+ (let [authlink-response  (test-app (ring-mock/request :get "/authlink"))
+       ring-session-val   (extract-ring-session-val authlink-response)
+
+       login-response     (make-session-get-request "/login" {} ring-session-val)
+       state              (extract-state-from-redirect-url login-response)
+
+       authlink-error  (make-session-get-request
+                        "/redirect" {:error "auth-error" :state state} ring-session-val)]
+
+   (:status authlink-response)                                   => 302
+   (re-find #"/login" (get-header authlink-response "Location")) => "/login"
+
+   (:status login-response)                                      => 302
+   (re-find #"/authenticate"
+            (get-header login-response "Location"))              => "/authenticate"
+            
+            
+    (:status authlink-error)                                     => 200
+    (:body authlink-error)                                       => "auth-error"))
+
 (future-fact
  "access-token-parsefn is used for the token if provided."
 
