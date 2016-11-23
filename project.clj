e17f5837d85bfc6d67853dc16e2265b7f4e56591
Duncan McGreggor <oubiwann@gmail.com>
2016-11-23 16:31:32 -0600

    Bumped version after last merge.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/friend-oauth2 "0.1.4"
+(defproject clojusc/friend-oauth2 "0.1.5"
   :description "OAuth2 workflow for Friend"
   :url "https://github.com/clojusc/friend-oauth2"
   :license {:name "MIT License"
