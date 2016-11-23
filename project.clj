a1d8f37a0dfba5e7ac24a6ec84953d4e6846d825
Duncan McGreggor <oubiwann@gmail.com>
2016-11-23 16:28:01 -0600

    Updated deps.

--- project.clj
+++ project.clj
@@ -1,23 +1,21 @@
-(defproject vita-io/friend-oauth2 "0.1.4"
-  :description "OAuth2 workflow for Friend (https://github.com/cemerick/friend). (Bug reports/contributions welcome!)"
+(defproject clojusc/friend-oauth2 "0.1.4"
+  :description "OAuth2 workflow for Friend"
   :url "https://github.com/clojusc/friend-oauth2"
   :license {:name "MIT License"
             :url "http://dd.mit-license.org"}
-
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.cemerick/friend "0.2.1" :exclusions [org.apache.httpcomponents/httpclient]]
-                 [ring "1.3.2"]
-                 [ring/ring-codec "1.0.0"]
-                 [clj-http "1.0.1"]
-                 [cheshire "5.4.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.cemerick/friend "0.2.3" :exclusions [org.apache.httpcomponents/httpclient]]
+                 [ring "1.5.0"]
+                 [ring/ring-codec "1.0.1"]
+                 [clj-http "3.4.1"]
+                 [cheshire "5.6.3"]
+                 [prismatic/schema "1.1.3"]
                  [crypto-random "1.2.0"]]
-
-  :plugins [[lein-midje "3.1.3"]
-            [codox "0.8.10"]]
-
+  :plugins [[lein-midje "3.2.1"]
+            [codox "0.10.2"]]
   :profiles {:dev
              {:dependencies [[ring-mock "0.1.5"]
-                             [org.clojure/tools.nrepl "0.2.5"]
-                             [midje "1.6.3"]
+                             [org.clojure/tools.nrepl "0.2.12"]
+                             [midje "1.8.3"]
                              [com.cemerick/url "0.1.1"]
-                             [compojure "1.3.1"]]}})
+                             [compojure "1.5.1"]]}})
