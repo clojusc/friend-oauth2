7423769801594180e6d7dbd6d025dfc45baaa049
Duncan McGreggor <oubiwann@gmail.com>
2016-12-04 23:30:36 -0600

    Updated to not use deprecated Compojure code.

--- docs/source/50-examples.md
+++ docs/source/50-examples.md
@@ -72,37 +72,68 @@ Here's the Google `friend-oauth2` example, runable from the commandline
             [clojure.tools.logging :as log]
             [clojusc.twig :as logger]
             [compojure.core :as compojure :refer [GET ANY defroutes]]
-            [compojure.handler :as handler]
             [friend-oauth2.service.google :as google]
             [friend-oauth2.util :as util]
-            [org.httpkit.server :as server])
+            [org.httpkit.server :as server]
+            [ring.util.response :as response]
+            [ring.middleware.defaults :as ring-defaults])
   (:gen-class))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Mini Webapp ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn html
+  [content]
+  (-> content
+      (response/response)
+      (response/header "Content-Type" "text/html")))
+
+(def index-content
+  (str "<a href=\"/admin\">Admin Pages</a><br />"
+       "<a href=\"/authlink\">Authorized page</a><br />"
+       "<a href=\"/authlink2\">Authorized page 2</a><br />"
+       "<a href=\"/status\">Status</a><br />"
+       "<a href=\"/logout\">Log out</a>"))
+
+(defn get-status-content
+  [count session]
+  (str "<p>We've hit the session page "
+       count
+       " times.</p><p>The current session: "
+       session
+       "</p>"))
+
+(defn render-index-page
+  [req]
+  (html index-content))
+
+(defn render-status-page
+  [req]
+  (let [count (:count (:session req) 0)
+        session (assoc (:session req) :count (inc count))]
+    (-> (get-status-content count session)
+        (html)
+        (assoc :session session))))
+
 (defroutes app-routes
-  (GET "/" request
-    (str "<a href=\"/admin\">Admin Pages</a><br />"
-         "<a href=\"/authlink\">Authorized page</a><br />"
-         "<a href=\"/authlink2\">Authorized page 2</a><br />"
-         "<a href=\"/status\">Status</a><br />"
-         "<a href=\"/logout\">Log out</a>"))
-  (GET "/status" request
-       (let [count (:count (:session request) 0)
-             session (assoc (:session request) :count (inc count))]
-         (-> (ring.util.response/response
-              (str "<p>We've hit the session page " (:count session)
-                   " times.</p><p>The current session: " session "</p>"))
-             (assoc :session session))))
-  (GET "/authlink" request
-       (friend/authorize #{::user} "Authorized page."))
-  (GET "/authlink2" request
-       (friend/authorize #{::user} "Authorized page 2."))
-  (GET "/admin" request
-       (friend/authorize #{::admin} "Only admins can see this page."))
-  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
+  (GET "/" req
+       (render-index-page req))
+  (GET "/status" req
+       (render-status-page req))
+  (GET "/authlink" req
+       (friend/authorize
+         #{::user}
+         (html "Authorized page.")))
+  (GET "/authlink2" req
+       (friend/authorize
+         #{::user}
+         (html "Authorized page 2.")))
+  (GET "/admin" req
+       (friend/authorize
+         #{::admin}
+         (html "Only admins can see this page.")))
+  (friend/logout (ANY "/logout" req (response/redirect "/"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; OAuth2 Configuration and Integration ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -130,7 +161,7 @@ Here's the Google `friend-oauth2` example, runable from the commandline
 (def app
   (-> app-routes
       (friend/authenticate auth-opts)
-      (handler/site)))
+      (ring-defaults/wrap-defaults ring-defaults/site-defaults)))
 
 (defn -main
   [& args]
--- examples/friend_oauth2/examples/github.clj
+++ examples/friend_oauth2/examples/github.clj
@@ -7,27 +7,37 @@
             [clojure.tools.logging :as log]
             [clojusc.twig :as logger]
             [compojure.core :as compojure :refer [GET ANY defroutes]]
-            [compojure.handler :as handler]
             [friend-oauth2.service.github :as github]
             [friend-oauth2.util :as util]
+            [org.httpkit.server :as server]
             [ring.util.response :as response]
-            [org.httpkit.server :as server])
+            [ring.middleware.defaults :as ring-defaults])
   (:gen-class))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Mini Webapp ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn render-status-page [request]
-  (let [count (:count (:session request) 0)
-        session (assoc (:session request) :count (inc count))]
-    (-> (str "<p>We've hit the session page "
-             (:count session)
-             " times.</p><p>The current session: "
-             session
-             "</p>")
-        (ring.util.response/response)
-        (assoc :session session))))
+(defn html
+  [content]
+  (-> content
+      (response/response)
+      (response/header "Content-Type" "text/html")))
+
+(def index-content
+  (str "<a href=\"/admin\">Admin Pages</a><br />"
+       "<a href=\"/repos\">My Github Repositories</a><br />"
+       "<a href=\"/authlink2\">Authorized page 2</a><br />"
+       "<a href=\"/status\">Status</a><br />"
+       "<a href=\"/logout\">Log out</a>"))
+
+(defn get-status-content
+  [count session]
+  (str "<p>We've hit the session page "
+       count
+       " times.</p><p>The current session:</p><p>"
+       session
+       "</p>"))
 
 (defn get-github-repos
   "Github API call for the current authenticated users repository list."
@@ -37,38 +47,60 @@
         repos (json/read-str (:body response) :key-fn keyword)]
     repos))
 
+(defn render-index-page
+  [req]
+  (html index-content))
+
+(defn render-status-page
+  [req]
+  (let [count (:count (:session req) 0)
+        session (assoc (:session req) :count (inc count))]
+    (-> (get-status-content count session)
+        (html)
+        (assoc :session session))))
+
 (defn render-repos-page
   "Shows a list of the current users github repositories by calling the github api
    with the OAuth2 access token that the friend authentication has retrieved."
-  [request]
-  (let [access-token (github/get-token request)
+  [req]
+  (let [access-token (github/get-token req)
         repos-response (get-github-repos access-token)]
     (log/debug "Got token:" access-token)
     (log/trace "Got repos:" (logger/pprint repos-response))
-    (->> repos-response
-         (map :name)
-         (vec)
-         (str))))
+    (html (->> repos-response
+               (map :name)
+               (vec)
+               (str)))))
 
 (defroutes app-routes
-  (GET "/" request
-    (str "<a href=\"/repos\">My Github Repositories</a><br />"
-         "<a href=\"/status\">Status</a><br />"
-         "<a href=\"/logout\">Log out</a>"))
-  (GET "/status" request
-       (render-status-page request))
-  (GET "/repos" request
-       (friend/authorize #{::user} (render-repos-page request)))
-  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
+  (GET "/" req
+       (render-index-page req))
+  (GET "/status" req
+       (render-status-page req))
+  (GET "/repos" req
+       (friend/authorize
+         #{::user}
+         (render-repos-page req)))
+  (GET "/authlink2" req
+       (friend/authorize
+         #{::user}
+         (html "Authorized page 2.")))
+  (GET "/admin" req
+       (friend/authorize
+         #{::admin}
+         (html "Only admins can see this page.")))
+  (friend/logout (ANY "/logout" req (response/redirect "/"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; OAuth2 Configuration and Integration ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(derive ::admin ::user)
+
 (defn credential-fn
   [token]
   ;;lookup token in DB or whatever to fetch appropriate :roles
-  {:identity token :roles #{::user}})
+  {:identity token :roles #{::admin}})
 
 (def workflow
   (github/workflow
@@ -87,7 +119,7 @@
 (def app
   (-> app-routes
       (friend/authenticate auth-opts)
-      (handler/site)))
+      (ring-defaults/wrap-defaults ring-defaults/site-defaults)))
 
 (defn -main
   [& args]
